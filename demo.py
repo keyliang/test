a4b3a6d5077c5dbfc1b74068d9d04fd50b39f672
lvchen <2285801343@qq.com>
2020-06-11 14:33:06 +0800

    第一次代码提交

--- demo.py
+++ demo.py
@@ -1,1 +1,16 @@
-print('hello world!')
+ print('hello world!')
+
+a=float(input('请输入：'))
+b=float(input('请输入:'))
+# print('输出结果为:' , a+b)
+a="你哈后啊"
+print(len(a))
+
+
+
+
+
+
+a=input('请输入一段内容:')
+b=input('输入第二行文字:')
+print('两段内容长度为：',len(a)+len(b))
